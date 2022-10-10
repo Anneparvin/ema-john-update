83e43300a302d0760ef94d4c04bef84a46c53d2b
Anneparvin <108586988+Anneparvin@users.noreply.github.com>
2022-10-11 04:06:40 +0600

    adde

---
+++ src/components/Step.js/Step.js
@@ -0,0 +1,25 @@
+// (Basic router step start,,,,,)
+
+// 1..npm i react-router-dom.
+// 2..create a router using createBrowserRouter.
+// 3..Add a RouterProvider And set Router app.
+// 4..Create a lot of Components.
+// 5..create main layout with some common part and one changing part based on the root.
+// 6..set main layout at the root of the route.
+// 7..set children routes.
+// 8..set link to the header section.
+
+//( // Basic router step end,,,,,)
+
+// (conditional rendaring steps start)
+
+// 1..use element in a variable and then use if-else to set value.
+// 2..ternary oparation condition? true :false;
+// 3..&& oparation (true condition)
+// 3..|| oparation (false condition)
+
+// (context Api)
+
+// 1..declare a context.
+// 2..set provider with value.
+// 3..useContext with the place where you want to get the context.
